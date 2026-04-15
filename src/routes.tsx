9539a0fbb5adcc7fbcd864d8714f8e7b17ed6157
Heitor Rosa <heitorrs.dev@gmail.com>
2026-04-15 10:38:49 -0300

    feat: add products page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { Produtos } from './pages/Produtos';
 import Placeholder from './pages/placeholder';
 
 export const router = createBrowserRouter([
@@ -9,7 +10,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Placeholder />,
+        element: <Produtos />,
       },
       {
         path: '/faturamento',
